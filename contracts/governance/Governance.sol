6a07fdaa6e0a66d6d63b26026a7e2b91d137f622
reyhanjulias <reyhanjoyoastro@gmail.com>
2025-08-04 09:24:26 +0700

    update

--- contracts/governance/Governance.sol
+++ contracts/governance/Governance.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import "../interfaces/IGovernance.sol";
+import "./interfaces/IGovernance.sol";
 
 /**
  * @title Governance
